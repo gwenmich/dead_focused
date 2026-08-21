aa5254f9f58ac21e8af4674fbe281a2b572b8d40
Gwen Michailidis <79330147+gwenmich@users.noreply.github.com>
2026-08-21 21:30:26 +0100

    Add play btn and vertical layout

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QWidget, QStackedLayout
+from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QWidget, QStackedLayout, QPushButton, QVBoxLayout
 from PySide6.QtCore import QSize, Qt
-from PySide6.QtGui import QMovie, QFontDatabase, QFont
+from PySide6.QtGui import QMovie, QFontDatabase, QIcon
 from pathlib import Path
 
 # set absolute paths for folders
@@ -37,16 +37,30 @@ class MainWindow(QMainWindow):
             font-weight: bold;
             background: transparent;""")
 
+        # play button
+        self.play_btn = QPushButton()
+        self.play_btn.setIcon(QIcon(str(IMAGES_DIR) + "/play.png"))
+        self.play_btn.setIconSize(QSize(32, 32))
+        self.play_btn.setFixedSize(32, 32)
+
         # stack background and timer
         scene = QWidget()
         scene.setFixedSize(512, 512)
-        stack = QStackedLayout(scene)
-        stack.setStackingMode(QStackedLayout.StackingMode.StackAll)
-        stack.addWidget(self.bg_label)
-        stack.addWidget(self.timer_label)
-        stack.setCurrentWidget(self.timer_label)
+        bg_stack = QStackedLayout(scene)
+        bg_stack.setStackingMode(QStackedLayout.StackingMode.StackAll)
+        bg_stack.addWidget(self.bg_label)
+        bg_stack.addWidget(self.timer_label)
+        bg_stack.setCurrentWidget(self.timer_label)
 
-        self.setCentralWidget(scene)
+        # set vertical box layout
+        app_layout = QWidget()
+        app_stack = QVBoxLayout(app_layout)
+        app_stack.setContentsMargins(0, 0, 0, 0)
+        app_stack.setAlignment(Qt.AlignmentFlag.AlignTop)
+        app_stack.addWidget(scene)
+        app_stack.addWidget(self.play_btn)
+
+        self.setCentralWidget(app_layout)
 
     # get pixel font
     def retrieve_font(self):
